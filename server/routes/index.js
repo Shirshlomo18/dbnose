3a213d3311649dbbf44ffedf30850016a8fd7ede
brachalevi <barlevi055@gmail.com>
2023-12-05 15:12:28 +0200

    not work server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const addStudent = () => {};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+// router.get('/student', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/teacher', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/admin', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/classroom', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// router.get('/classroom', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/*add*/
+router.post("/", function (req, res, next) {
+  const body = req.body;
+
+  res.render("index", { title: "Express" });
+});
+router.post("/student", function (req, res, next) {
+  const body = req.body;
+  addStudent(body).then((data) => {
+    res.render("index", data);
+  });
+});
+router.post("/teacher", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.post("/admin", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.post("/classroom", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.post("/classroom", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
