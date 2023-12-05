eb6f4015a73e7296eb318809f818955cef8bff54
brachalevi <barlevi055@gmail.com>
2023-12-05 17:28:44 +0200

    finish

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const addStudent = () => {};
 const Admin = require("../../DB/admin/admin");
-const {Student, getStudent} = require("../../DB/student/student");
-const {Classroom,getClassroom, getStudentByClassroom} = require("../../DB/classroom/classroom");
+const { Student, getStudent } = require("../../DB/student/student");
+const {
+  Classroom,
+  getClassroom,
+  getStudentByClassroom,
+} = require("../../DB/classroom/classroom");
 const Teacher = require("../../DB/teacher/teacher");
 const School = require("../../DB/school/school.js");
 
@@ -11,12 +14,7 @@ const School = require("../../DB/school/school.js");
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
-router.get("/student", function (req, res, next) {
-  // getStudent(body).then((data) => {
-  //   console.log("data: ", data);
-  //   res.send(JSON.stringify(data));
-  // });
-});
+
 // router.get('/teacher', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
@@ -31,15 +29,17 @@ router.get("/student", function (req, res, next) {
 // });
 
 //get all
-router.get('/student' ,(req,res)=>{
-  getStudent();
-  res.send();
+router.get("/student", (req, res) => {
+  getStudent().then((data) => {
+    console.log("data: ", data);
+    res.send(JSON.stringify(data));
+  });
 });
-router.get('/classroomandadmins' ,(req,res)=>{
+router.get("/classroomandadmins", (req, res) => {
   getClassroom();
   res.send();
 });
-router.get('/studentbyclassroom' ,(req,res)=>{
+router.get("/studentbyclassroom", (req, res) => {
   getStudentByClassroom();
   res.send();
 });
