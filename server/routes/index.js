87b5e9180168c160400bc0bbf5aea0139d6f9ab3
brachalevi <barlevi055@gmail.com>
2023-12-05 16:24:49 +0200

    server posts

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const router = express.Router();
 const addStudent = () => {};
 const Admin = require("../../DB/admin/admin");
+const Student = require("../../DB/student/student");
+const Classroom = require("../../DB/classroom/classroom");
+const Teacher = require("../../DB/teacher/teacher");
+const School = require("../../DB/school/school");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
-// router.get('/student', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get("/student", function (req, res, next) {
+  // getStudent(body).then((data) => {
+  //   console.log("data: ", data);
+  //   res.send(JSON.stringify(data));
+  // });
+});
 // router.get('/teacher', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
@@ -32,29 +39,42 @@ router.post("/", function (req, res, next) {
 router.post("/student", function (req, res, next) {
   const body = req.body;
   console.log("body: ", body);
-  addStudent(body).then((data) => {
-    res.render("index", data);
+  Student(body).then((data) => {
+    console.log("data: ", data);
+    res.send(JSON.stringify(data));
   });
 });
 router.post("/teacher", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  const body = req.body;
+  console.log("body: ", body);
+  Teacher(body).then((data) => {
+    console.log("data: ", data);
+    res.send(JSON.stringify(data));
+  });
 });
 router.post("/admin", function (req, res, next) {
   const body = req.body;
-  console.log("body: ", body);
-
   console.log("Admin: ", Admin);
   Admin(body).then((data) => {
     console.log("data: ", data);
     res.send(JSON.stringify(data));
-    // res.render("index!!");
   });
 });
 router.post("/classroom", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  const body = req.body;
+  console.log("body: ", body);
+  Classroom(body).then((data) => {
+    console.log("data: ", data);
+    res.send(JSON.stringify(data));
+  });
 });
-router.post("/classroom", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.post("/school", function (req, res, next) {
+  const body = req.body;
+  console.log("body: ", body);
+  School(body).then((data) => {
+    console.log("data: ", data);
+    res.send(JSON.stringify(data));
+  });
 });
 
 module.exports = router;
